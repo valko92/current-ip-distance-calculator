016de8842d08db0487ef82bcea1b6e23877fcfca
Valeriya K <valko92@hotmail.com>
2017-12-08 01:11:11 -0500

    keep messing up CORS header b/c I do it diff each time >.<

--- rest-service/index.js
+++ rest-service/index.js
@@ -7,7 +7,8 @@ var http = require('http').Server(app);
 var getdistance = require('./getdistance.js');
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000, http://val.kozhynov.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://val.kozhynov.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
 
     next();
